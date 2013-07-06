7d46589ae86a2770353338132f9df1f48fd0b1a9
Mike Martinez <brillozon@gmail.com>
2013-07-06 04:03:15 +0000

    Update grader for Part 3.

--- grader.js
+++ grader.js
@@ -5,6 +5,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -17,16 +18,25 @@ var assertFileExists = function(infile) {
   return instr;
 }
 
-var cheerioHtmlFile = function(htmlfile) {
+var cheerioLoadFile = function(htmlfile) {
   return cheerio.load(fs.readFileSync(htmlfile));
+}
+
+var cheerioLoadUrl = function(html) {
+  return cheerio.load(html);
 }
 
 var loadChecks = function(checksfile) {
   return JSON.parse(fs.readFileSync(checksfile));
 }
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
+var generateOutput = function(result) {
+  var resultOutput = JSON.stringify(result, null, 4);
+  console.log(resultOutput);
+}
+
+var checkHtml = function(htmlfile, checksfile, htmlloader) {
+  $ = htmlloader(htmlfile);
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for(var ii in checks) {
@@ -38,12 +48,30 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 
 if(require.main == module) {
   program.option('-c, --checks <file>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-         .option('-f,  --file <file>', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+         .option('-u,  --url [url]', 'URL of index.html')
+         .option('-f,  --file [file]', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
          .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+
+  var resultJson;
+  if( program.url) {
+    rest.get(program.url.toString()).on('complete', function(result) {
+      if( result instanceof Error) {
+        console.log("Failed to fetch URL: " + program.url);
+        process.exit(1);
+      }
+      // This is asynchronous, so we can't simplly fall out and finish.
+      resultJson = checkHtml(result, program.checks, cheerioLoadUrl);
+      generateOutput(resultJson);
+    });
+
+  } else {
+    resultJson = checkHtml(program.file, program.checks, cheerioLoadFile);
+    generateOutput(resultJson);
+  }
+
 } else {
-  exports.checkHtmlFile = checkHtmlFile;
+  exports.checkHtmlFile = function(htmlfile, checksfile) {
+      return checkHtml(htmlfile, checksfile, cheerioLoadFile);
+    };
 }
 
